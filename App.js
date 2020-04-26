1639a44f8dd47a6455af91180507f1bc222b272a
Junior Soares <34192870+JuniorSoares716@users.noreply.github.com>
2020-04-26 19:49:56 -0300

    Application Mobile

---
+++ App.js
@@ -0,0 +1,13 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Remote debugger']);
+
+import Routes from './src/routes'; 
+
+export default function App() {
+  return (
+   <Routes/>
+  );
+}
